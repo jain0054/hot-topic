9ab09f9211f31a2e7cce5723e7377493128c1a8a
jain0054 <122372524+jain0054@users.noreply.github.com>
2023-04-18 21:34:25 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const container = document.querySelector("#main");
+const links = document.querySelectorAll(".nav");
+let url = "partials/home.html";
+
+const loadContent = (urlFeed) => {
+    fetch(urlFeed)
+        .then((response) => {
+            return response.text();
+        })
+        .then((data) => {
+            container.innerHTML = data;
+        })
+        .catch((error) => {
+            console.error("Error fetching content: ", error);
+        });
+};
+
+const selectContent = (event) => {
+    event.preventDefault();
+    url = event.target.href;
+    loadContent(url);
+};
+
+loadContent(url);
+
+links.forEach((link) => {
+    link.addEventListener("click", selectContent);
+});
